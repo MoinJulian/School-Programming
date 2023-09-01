3a32fec434c6065d37106823fda5b89724e0968d
MoinJulian <100045739+MoinJulian@users.noreply.github.com>
2023-09-01 20:42:26 +0200

    Update Task_1_new.py

--- CourseWork/Task_1_new.py
+++ CourseWork/Task_1_new.py
@@ -1,15 +1,34 @@
-name = input ('Please enter your name')
-volunteer = int(input('Would you like to be a volunteer for the Friends of Seaview Pier? Select 1 for yes, 2 for no'))
-while volunteer != 1 and volunteer != 2:
-    volunteer = int(input('Would you like to be a volunteer for the Friends of Seaview Pier? Select 1 for yes, 2 for no'))
-if volunteer != 1 and volunteer != 2:
-    print ('That is not a valid number.')
-   
-while volunteer == 2:
-    donation = int(input('Would you like to donate to the Friends of The Seaview Pier? If yes, choose 1, if no, choose 2'))
+def get_valid_input(prompt, valid_values):
+    while True:
+        user_input = input(prompt)
+        if user_input in valid_values:
+            return user_input
+        print("Invalid input. Please try again.")
 
-if volunteer == 1:
-    areaofwork = int(input('Where would you like to work? Choose the number 1 for pier enterance, 2 for the gift shop, or 3 for painting and decorating.'))
-dayenter = int(input('What is the current day? Enter as 1-31'))
-monthenter = int(input('What is the current month? Enter as 1-12'))
-yearenter = int(input('What is the current year? Enter 2010 - 2060'))
+name = input("Please enter your name: ")
+
+valid_volunteer_values = ["1", "2"]
+volunteer = get_valid_input(
+    "Would you like to be a volunteer for the Friends of Seaview Pier?"
+    " Select 1 for yes, 2 for no: ",
+    valid_volunteer_values
+)
+
+if volunteer == "2":
+    donation = get_valid_input(
+        "Would you like to donate to the Friends of The Seaview Pier?"
+        " If yes, choose 1, if no, choose 2: ",
+        valid_volunteer_values
+    )
+    if donation == "2":
+        print("Okay, you've been registered!")
+
+if volunteer == "1":
+    area_of_work = input(
+        "Where would you like to work? Choose the number 1 for pier entrance,"
+        " 2 for the gift shop, or 3 for painting and decorating: "
+    )
+
+day_enter = int(input("What is the current day? Enter a number between 1 and 31: "))
+month_enter = int(input("What is the current month? Enter a number between 1 and 12: "))
+year_enter = int(input("What is the current year? Enter a number between 2010 and 2060: "))
