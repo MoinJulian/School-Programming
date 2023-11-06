72515e33bf87a9f4aaa89a4bd0330f0ec5a9e7a8
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-11-06 16:19:11 +0000

    Add Menu edit files

---
+++ Write Files/menu.py
@@ -0,0 +1,14 @@
+#Open a text file for writing
+
+def edit_file(firstname, surname, password):
+    text_file = open("/Users/julianhammer/Library/Mobile Documents/com~apple~CloudDocs/Dev/Python School/Write Files/names.txt", "w")
+    text_file.write("You're first name is: " + firstname + "\n")
+    text_file.write("Your Surname is" + surname + "\n")
+    text_file.write("Your Password is: " + password + "\n")
+    text_file.close()
+
+firstname = input("Please enter your Firstname: ")
+surname = input("Please enter your Surname: ")
+password = input("Please enter a Pasword: ")
+
+edit_file(firstname, surname, password)
