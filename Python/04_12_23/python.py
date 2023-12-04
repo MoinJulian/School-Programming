1293d8cd8e1fda0167ec8edd3ab6d8f73b72ee2a
Julian Hammer <100045739+MoinJulian@users.noreply.github.com>
2023-12-04 16:01:58 +0000

    Add more question

--- Python/04_12_23/python.py
+++ Python/04_12_23/python.py
@@ -1,15 +1,18 @@
 # Define a list or dictionary to store questions along with correct answers
 questions = [
-    {"question": "What is the capital of France?", "correct_answer": "Paris"},
+    {"question": "What is 4 x 10?", "correct_answer": "40"},
     {"question": "What is 2 + 2?", "correct_answer": "4"},
-    # Add more questions as needed
+    {"question": "What is 30 - 25.4?", "correct_answer": "4.6"},
+    {"question": "What is 15 / 5?", "correct_answer": "3"},
+    {"question": "What is the sum of the first 50th term of folowing arimethic sequence? \n10+18+36", "correct_answer": "10300"},
+    {"question": "What is the square root of π^2?", "correct_answer": "Pi"}
 ]
 
 # Create a variable to keep track of the user's correct answers
 correct_answers_count = 0
 
 # Create a file to save the answered questions
-with open("/Users/julianhammer/Library/Mobile Documents/com~apple~CloudDocs/Dev/Python School/04_12_23/answered_questions.txt", "w") as file:
+with open("/Users/julianhammer/Library/Mobile Documents/com~apple~CloudDocs/Dev/Python School/Python/04_12_23/answered_questions.txt", "w") as file:
     # Iterate through the list of questions
     for question_data in questions:
         question = question_data["question"]
